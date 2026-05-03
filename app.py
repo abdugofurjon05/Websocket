91f32ee49f95d70b29fc775be5c4ae2f4f940dba
Abdugofurjon05 <gofurjonshuxratov05@gmail.com>
2026-05-03 11:34:36 +0500

    Implemented Websockets

--- app.py
+++ app.py
@@ -1,6 +1,31 @@
-def main():
-    print("Hello from websocket!")
+import asyncio
+from websockets.asyncio.server import serve
 
 
-if __name__ == "__main__":
-    main()
+async def websocket_handler(websocket):
+    websocket_id = str(websocket.id)[:6]
+
+    
+    print(f"Client connected: {websocket_id}")
+
+    try:
+        async for message in websocket: 
+            await websocket.send(f"Echo: {message}")
+    except Exception as e:
+        print(f"Error with client {websocket_id}: {e}")
+    finally:
+        print(f"Client disconnected: {websocket_id}")
+
+
+
+ 
+async def main():
+    async with serve(websocket_handler, "localhost", 8765):
+        print(f"WebSocket server started on ws://localhost:8765" )
+        await asyncio.Future()  # Run forever
+
+
+asyncio.run(main())
+
+
+ 
